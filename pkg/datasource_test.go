07cf0ce0051e98cefca383a708e8e696020d3f13
Brian Gann <bkgann@gmail.com>
2020-07-25 14:25:09 -0500

    placeholder

---
+++ pkg/datasource_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// TestPlugin checks plugin init
+func TestDataSource(t *testing.T) {
+	Convey("Placeholder", t, func() {
+		So(true, ShouldBeTrue)
+	})
+}
